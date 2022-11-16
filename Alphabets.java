638e682ee0e1739efd6d6c6a8b633fd88a16edc2
Leslie Clottey <tcclotteygh@gmail.com>
2022-11-15 23:03:58 -0500

    Create Alphabets.java
    Java Code testing the use of an array to output letters of the alphabet

---
+++ Alphabets.java
@@ -0,0 +1,16 @@
+package myFirstJavaProject;
+
+public class Alphabets{
+
+	public static void main(String[] args) {
+				
+    /** Creates a new instance of Array  */
+    
+      char alphabets[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+  		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+      for (int i=0; i<(alphabets.length); i++ ) {
+    	  System.out.print(alphabets[i] + " ");
+      }
+	}
+	
+}
